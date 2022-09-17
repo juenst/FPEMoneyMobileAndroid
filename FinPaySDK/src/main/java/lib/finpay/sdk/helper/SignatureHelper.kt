9bf9c79b2529d194603e5068c369e1f0bc81db0d
Widi Ramadhan <widiyanto.ramadhan@gmail.com>
2022-09-17 22:04:44 +0700

    test

--- FinPaySDK/src/main/java/lib/finpay/sdk/helper/SignatureHelper.kt
+++ FinPaySDK/src/main/java/lib/finpay/sdk/helper/SignatureHelper.kt
@@ -1,13 +1,25 @@
 package lib.finpay.sdk.helper
 
+//import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.binary.Hex
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+//import org.apache.commons.codec.binary.Hex
+
 
 class SignatureHelper {
+    @RequiresApi(Build.VERSION_CODES.O)
     fun createSignature(): String {
         val sorted = param().toList().sortedBy { (key, _) -> key}.toMap()
         val joinedSorted =sorted.values.joinToString("").uppercase()
-        return sorted.toString();
+        val key = "daYumnMb"
+        val signature = createSignatures(joinedSorted, key)
+        print(joinedSorted)
+        print(signature)
+        return signature;
     }
 
     fun param(): Map<String, Any> {
@@ -20,5 +32,17 @@ class SignatureHelper {
             "transNumber" to currentDate
         )
         return mapJson;
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createSignatures(data: String, key: String): String {
+        val sha256Hmac = Mac.getInstance("HmacSHA256")
+        val secretKey = SecretKeySpec(key.toByteArray(), "HmacSHA256")
+        sha256Hmac.init(secretKey)
+
+//        return Hex.encodeHexString(sha256Hmac.doFinal(data.toByteArray()))
+
+        // For base64
+         return Base64.getEncoder().encodeToString(sha256Hmac.doFinal(data.toByteArray()))
     }
 }
