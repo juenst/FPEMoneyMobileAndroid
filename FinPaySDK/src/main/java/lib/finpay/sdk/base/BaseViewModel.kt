c25d655fcc8e32aeb3c204979402757bf57a4cb7
Muhammad Rasyiddin <rasyid@192.168.1.3>
2022-09-19 22:25:03 +0700

    add: gradle, base view model

--- FinPaySDK/src/main/java/lib/finpay/sdk/base/BaseViewModel.kt
+++ FinPaySDK/src/main/java/lib/finpay/sdk/base/BaseViewModel.kt
@@ -1,13 +1,26 @@
 package lib.finpay.sdk.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
 open class BaseViewModel : ViewModel(), HasAndroidInjector{
 
-    override fun androidInjector(): AndroidInjector<Any> {
-        TODO("Not yet implemented")
+    @Inject
+//    lateinit var api: Api
+    private val loadingLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    fun setLoading(state: Boolean){
+        loadingLiveData.postValue(state)
     }
+
+    fun isLoading(): LiveData<Boolean> = loadingLiveData
+
+    @Inject
+    lateinit var dispactchingActivityInjector: DispatchingAndroidInjector<Any>
+    override fun androidInjector(): AndroidInjector<Any> = dispactchingActivityInjector
 
 }
