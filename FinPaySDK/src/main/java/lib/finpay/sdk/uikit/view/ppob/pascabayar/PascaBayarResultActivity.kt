cc5be823f2f01b1db70c7f1f0c3528b451a4990d
Widi Ramadhan <widiyanto.ramadhan@gmail.com>
2022-10-22 23:44:51 +0700

    add result PLN dan pascabayar

---
+++ FinPaySDK/src/main/java/lib/finpay/sdk/uikit/view/ppob/pascabayar/PascaBayarResultActivity.kt
@@ -0,0 +1,34 @@
+package lib.finpay.sdk.uikit.view.ppob.pascabayar
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import lib.finpay.sdk.R
+import lib.finpay.sdk.corekit.FinpaySDK
+import lib.finpay.sdk.corekit.model.DataSubProduct
+import lib.finpay.sdk.uikit.constant.PaymentType
+import lib.finpay.sdk.uikit.utilities.ButtonUtils
+import lib.finpay.sdk.uikit.utilities.DialogUtils
+import lib.finpay.sdk.uikit.utilities.TextUtils
+import lib.finpay.sdk.uikit.utilities.Utils
+import lib.finpay.sdk.uikit.view.payment.PaymentActivity
+import lib.finpay.sdk.uikit.view.ppob.pulsa_data.adapter.PulsaDataAdapter
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class PascaBayarResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pascabayar)
+        supportActionBar!!.hide()
+    }
+}
