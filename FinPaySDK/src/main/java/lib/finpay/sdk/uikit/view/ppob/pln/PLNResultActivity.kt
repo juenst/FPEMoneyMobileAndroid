8ad1045f1a9d8fbab78769253874667366e9d277
Muhammad Rasyiddin <rasyid@192.168.1.2>
2022-10-26 15:33:54 +0700

    fix: pln

--- FinPaySDK/src/main/java/lib/finpay/sdk/uikit/view/ppob/pln/PLNResultActivity.kt
+++ FinPaySDK/src/main/java/lib/finpay/sdk/uikit/view/ppob/pln/PLNResultActivity.kt
@@ -2,32 +2,28 @@ package lib.finpay.sdk.uikit.view.ppob.pln
 
 import android.app.ProgressDialog
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import lib.finpay.sdk.R
 import lib.finpay.sdk.corekit.FinpaySDK
 import lib.finpay.sdk.corekit.constant.ProductCode
-import lib.finpay.sdk.corekit.model.DataSubProduct
-import lib.finpay.sdk.corekit.model.PpobInquiry
 import lib.finpay.sdk.uikit.constant.PaymentType
-import lib.finpay.sdk.uikit.utilities.ButtonUtils
 import lib.finpay.sdk.uikit.utilities.DialogUtils
+import lib.finpay.sdk.uikit.utilities.SharedPrefKeys
 import lib.finpay.sdk.uikit.utilities.TextUtils
-import lib.finpay.sdk.uikit.utilities.Utils
 import lib.finpay.sdk.uikit.view.payment.PaymentActivity
-import lib.finpay.sdk.uikit.view.ppob.pulsa_data.adapter.PulsaDataAdapter
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class PLNResultActivity : AppCompatActivity() {
-//    val inquiry = intent.getSerializableExtra("result") as? PpobInquiry
+    //    val inquiry = intent.getSerializableExtra("result") as? PpobInquiry
     val noPelanggan: String? by lazy { intent.getStringExtra("noPelanggan") }
     val customerName: String? by lazy { intent.getStringExtra("customerName") }
     val customerId: String? by lazy { intent.getStringExtra("customerId") }
@@ -40,7 +36,7 @@ class PLNResultActivity : AppCompatActivity() {
     lateinit var txtDaya: TextView
     lateinit var txtTagihan: TextView
     lateinit var btnBack: ImageView
-    lateinit var btnConfirm: ImageView
+    lateinit var btnConfirm: Button
     lateinit var progressDialog: ProgressDialog
 
     var saldo: String = "0"
@@ -60,8 +56,11 @@ class PLNResultActivity : AppCompatActivity() {
         btnConfirm = findViewById(R.id.btnConfirm)
         progressDialog = ProgressDialog(this@PLNResultActivity)
 
+        FinpaySDK.init(this)
+
         txtCustName.text = customerName
-        txtCustPhoneNumber.text = noPelanggan
+        txtCustPhoneNumber.text =
+            FinpaySDK.prefHelper.getStringFromShared(SharedPrefKeys.USER_PHONE_NUMBER)
         txtCustId.text = customerId
         txtDaya.text = "-"
         txtTagihan.text = TextUtils.formatRupiah(tagihan!!.toDouble())
@@ -85,7 +84,7 @@ class PLNResultActivity : AppCompatActivity() {
         FinpaySDK.getUserBallance(this@PLNResultActivity, {
             saldo = it.amount!!
             progressDialog.dismiss()
-        },{
+        }, {
             progressDialog.dismiss()
             DialogUtils.showDialogError(this@PLNResultActivity, "", it)
         })
@@ -98,26 +97,27 @@ class PLNResultActivity : AppCompatActivity() {
         val txtBtnPay = dialog.findViewById<TextView>(R.id.txtBtnPay)
         val txtDenom = dialog.findViewById<TextView>(R.id.txtDenom)
         val txtPrice = dialog.findViewById<TextView>(R.id.txtPrice)
-        val txtBiayaLayanan= dialog.findViewById<TextView>(R.id.txtBiayaLayanan)
-        val txtTotalBayar= dialog.findViewById<TextView>(R.id.txtTotalBayar)
-        val txtSaldo= dialog.findViewById<TextView>(R.id.saldo)
-        val cardWarning= dialog.findViewById<CardView>(R.id.cardWarning)
-        val logoProvider= dialog.findViewById<CardView>(R.id.logoProvider)
+        val txtBiayaLayanan = dialog.findViewById<TextView>(R.id.txtBiayaLayanan)
+        val txtTotalBayar = dialog.findViewById<TextView>(R.id.txtTotalBayar)
+        val txtSaldo = dialog.findViewById<TextView>(R.id.saldo)
+        val cardWarning = dialog.findViewById<CardView>(R.id.cardWarning)
+        val logoProvider = dialog.findViewById<ImageView>(R.id.logoProvider)
 
 
-        if(saldo.toInt() < (tagihan!!.toInt() + fee!!.toInt())){
+        if (saldo.toInt() < (tagihan!!.toInt() + fee!!.toInt())) {
             cardWarning!!.visibility = View.VISIBLE
             txtBtnPay!!.text = "Isi Saldo"
         }
         logoProvider!!.visibility = View.GONE
-        txtDenom!!.text = "Token PLN "+TextUtils.formatRupiah(tagihan!!.toDouble())
+        txtDenom!!.text = "Token PLN " + TextUtils.formatRupiah(tagihan!!.toDouble())
         txtPrice!!.text = TextUtils.formatRupiah(tagihan!!.toDouble())
         txtBiayaLayanan!!.text = TextUtils.formatRupiah(fee!!.toDouble())
-        txtTotalBayar!!.text = TextUtils.formatRupiah((tagihan!!.toInt() + fee!!.toInt()).toDouble())
+        txtTotalBayar!!.text =
+            TextUtils.formatRupiah((tagihan!!.toInt() + fee!!.toInt()).toDouble())
         txtSaldo!!.text = TextUtils.formatRupiah(saldo.toDouble())
 
         btnPay?.setOnClickListener {
-            if(saldo.toInt() < (tagihan!!.toInt() + fee!!.toInt())){
+            if (saldo.toInt() < (tagihan!!.toInt() + fee!!.toInt())) {
                 //open top up
             } else {
                 progressDialog.setTitle("Mohon Menunggu")
@@ -128,7 +128,7 @@ class PLNResultActivity : AppCompatActivity() {
                 val currentDate = sdf.format(Date())
                 FinpaySDK.authPin(
                     this@PLNResultActivity,
-                    tagihan!!.toString(), ProductCode.PLN_POSTPAID,{
+                    tagihan!!.toString(), ProductCode.PLN_POSTPAID, {
                         progressDialog.dismiss()
                         val intent = Intent(this@PLNResultActivity, PaymentActivity::class.java)
                         intent.putExtra("paymentType", PaymentType.paymentPPOB)
@@ -137,10 +137,11 @@ class PLNResultActivity : AppCompatActivity() {
                         intent.putExtra("denom", tagihan!!)
                         intent.putExtra("reffFlag", nomorReferensi)
                         intent.putExtra("billingId", noPelanggan)
-                        intent.putExtra("productCode", ProductCode.PLN_POSTPAID)
+                        intent.putExtra("productCode", ProductCode.PLN_PREPAID)
                         intent.putExtra("activationDate", currentDate)
                         intent.putExtra("payType", "billpayment")
                         intent.putExtra("widgetURL", it.widgetURL)
+                        intent.putExtra("phoneNumber", txtCustPhoneNumber.text.toString())
                         startActivity(intent)
                     }, {
                         progressDialog.dismiss()
