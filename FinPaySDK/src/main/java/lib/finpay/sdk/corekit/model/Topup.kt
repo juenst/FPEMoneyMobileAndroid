b4abf6df0e995ab1af12b92af85c0f862e090be6
Yudhistira Caraka <raka.naruto4567@gmail.com>
2023-01-12 02:58:09 +0700

    update temporary topup not yet finished

--- FinPaySDK/src/main/java/lib/finpay/sdk/corekit/model/Topup.kt
+++ FinPaySDK/src/main/java/lib/finpay/sdk/corekit/model/Topup.kt
@@ -1,3 +1,3 @@
 package lib.finpay.sdk.corekit.model
 
-class Topup (val icon:Int, val name:String)
+class Topup (val icon:Int, val name:String,val sof:String)
--- FinPaySDK/src/main/java/lib/finpay/sdk/uikit/view/topup/TopupActivity.kt
+++ FinPaySDK/src/main/java/lib/finpay/sdk/uikit/view/topup/TopupActivity.kt
@@ -1,5 +1,7 @@
 package lib.finpay.sdk.uikit.view.topup
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.ImageView
@@ -7,8 +9,14 @@ import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import lib.finpay.sdk.R
+import lib.finpay.sdk.corekit.FinpaySDK
+import lib.finpay.sdk.corekit.constant.ProductCode
 import lib.finpay.sdk.corekit.model.Topup
+import lib.finpay.sdk.uikit.FinpaySDKUI.Companion.progressDialog
+import lib.finpay.sdk.uikit.constant.PaymentType
 import lib.finpay.sdk.uikit.helper.FinpayTheme
+import lib.finpay.sdk.uikit.utilities.DialogUtils
+import lib.finpay.sdk.uikit.view.payment.PaymentActivity
 import lib.finpay.sdk.uikit.view.topup.adapter.TopupAdapter
 
 class TopupActivity : AppCompatActivity()  {
@@ -16,6 +24,7 @@ class TopupActivity : AppCompatActivity()  {
     lateinit var listChannel: ListView
     lateinit var appbar: androidx.appcompat.widget.Toolbar
     lateinit var appbarTitle: TextView
+    lateinit var progressDialog: ProgressDialog
 
     val finpayTheme: FinpayTheme? by lazy { if(intent.getSerializableExtra("theme") == null) null else intent.getSerializableExtra("theme") as FinpayTheme }
     val transNumber: String? by lazy { if(intent.getStringExtra("transNumber") == null) "" else intent.getStringExtra("transNumber")}
@@ -29,6 +38,7 @@ class TopupActivity : AppCompatActivity()  {
         appbarTitle = findViewById(R.id.appbar_title)
         btnBack = findViewById(R.id.btnBack)
         listChannel = findViewById(R.id.list_channel)
+        progressDialog = ProgressDialog(this@TopupActivity)
 
         //theming
         appbar.setBackgroundColor(if(finpayTheme?.getAppBarBackgroundColor() == null)  Color.parseColor("#00ACBA") else finpayTheme?.getAppBarBackgroundColor()!!)
@@ -36,18 +46,53 @@ class TopupActivity : AppCompatActivity()  {
         btnBack.setColorFilter(if(finpayTheme?.getAppBarTextColor() == null)  Color.parseColor("#FFFFFF") else finpayTheme?.getAppBarTextColor()!!)
 
         var list = mutableListOf<Topup>()
-        list.add(Topup(R.drawable.ic_logo_pos_indonesia, "Pos Indonesia"))
-        list.add(Topup(R.drawable.ic_logo_alfamart, "Alfamart"))
-        list.add(Topup(R.drawable.ic_logo_mandiri, "Mandiri"))
-        list.add(Topup(R.drawable.ic_logo_briva, "BRIVA"))
-        list.add(Topup(R.drawable.ic_logo_bca, "Bank Central Asia"))
-        list.add(Topup(R.drawable.ic_logo_pegadaian, "Pegadaian"))
-        list.add(Topup(R.drawable.ic_logo_permata_bank, "Permata Bank"))
-        list.add(Topup(R.drawable.ic_logo_bnc, "BNC Bank"))
+        list.add(Topup(R.drawable.ic_logo_bni, "BNI","vabni"))
+        list.add(Topup(R.drawable.ic_logo_pos_indonesia, "Pos Indonesia","pos"))
+        list.add(Topup(R.drawable.ic_logo_alfamart, "Alfamart","alfa"))
+        list.add(Topup(R.drawable.ic_logo_mandiri, "Mandiri","vamandiri"))
+        list.add(Topup(R.drawable.ic_logo_briva, "BRIVA","briva"))
+        list.add(Topup(R.drawable.ic_logo_bca, "Bank Central Asia","vabca"))
+        list.add(Topup(R.drawable.ic_logo_pegadaian, "Pegadaian","pegadaian"))
+        list.add(Topup(R.drawable.ic_logo_permata_bank, "Permata Bank","vapermata"))
+        list.add(Topup(R.drawable.ic_logo_bnc, "BNC Bank","vabnbc"))
         listChannel.adapter = TopupAdapter(this,R.layout.item_topup,list)
         listChannel.setOnItemClickListener{paymentAdapter, view, position, id ->
-            val selectedItem = paymentAdapter.getItemAtPosition(position)
-            val itemIdAtPos = paymentAdapter.getItemIdAtPosition(position)
+
+            var amounts:String = "0"
+            var types:String = list.get(position).name
+
+            if (position != 0 || position != 7 || position != 8){
+                amounts = "0"
+            }else{
+
+            }
+
+            progressDialog.setTitle("Mohon Menunggu")
+            progressDialog.setMessage("Sedang Memuat ...")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+
+            println("sof : ${list.get(position).sof}")
+
+
+            FinpaySDK.topup(
+                transNumber!!,
+                this,
+                amounts,
+                list.get(position).sof, {
+                    progressDialog.dismiss()
+                    val intent = Intent(this, TopupGuidanceActivity::class.java)
+                    intent.putExtra("result", it)
+                    intent.putExtra("transNumber", transNumber!!)
+                    intent.putExtra("type", types)
+                    intent.putExtra("theme", finpayTheme)
+                    startActivity(intent)
+                    println("berhasil topup")
+                }, {
+                    progressDialog.dismiss()
+                    DialogUtils.showDialogError(this, "", it, finpayTheme)
+                }
+            )
 
             //Toast.makeText(this@,"Anda memilih item ke $itemIdAtPos",Toast.LENGTH_SHORT).show()
         }
